684fbebf540442f26c5edced091d726d25a2d7ac
Mark van der Tol <mark@plotprojects.com>
2019-01-29 14:44:43 +0100

    Integrate Plot Projects plugin

--- android/app/src/main/kotlin/com/plotprojects/example/plotprojectsflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/plotprojects/example/plotprojectsflutter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.plotprojects.example.plotprojectsflutter
 
 import android.os.Bundle
+import com.plotprojects.retail.android.Plot
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -9,5 +10,7 @@ class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+
+    Plot.init(this)
   }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,21 @@
 import UIKit
 import Flutter
+import PlotProjects
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, PlotDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    #if DEBUG
+    PlotDebug.initialize(launchOptions: launchOptions, delegate: self)
+    #else
+    PlotRelease.initialize(launchOptions: launchOptions, delegate: self)
+    #endif
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
